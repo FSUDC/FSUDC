733146cee21a50fc111d19d9f2cf1a61e259d322
ang618 <aliciag235@gmail.com>
2014-03-19 18:46:55 -0500

    Update UCLogin.java

--- src/com/vaadin/fsudc/UCLogin.java
+++ src/com/vaadin/fsudc/UCLogin.java
@@ -2,10 +2,9 @@ package com.vaadin.fsudc;
 
 import java.sql.SQLException;
 import java.util.Properties;
-
-import com.jcraft.jsch.JSch;
-import com.jcraft.jsch.JSchException;
-import com.jcraft.jsch.Session;
+import java.util.StringTokenizer;
+import javax.mail.*;
+import javax.mail.internet.*;
 import com.vaadin.server.Page;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Notification;
@@ -15,9 +14,8 @@ import com.vaadin.ui.Button.ClickEvent;
 public class UCLogin {
 	
 	private LoginUI loginForm = new LoginUI();
+	private VerifyUI verifyForm = new VerifyUI();
 	private TblMember dbTable;
-	private JSch jsch = new JSch();
-	private String host = "shell.cs.fsu.edu"; 
 	
 	@SuppressWarnings("serial")
 	public UCLogin() {
@@ -27,62 +25,125 @@ public class UCLogin {
 		loginForm.loginButton.addClickListener(new Button.ClickListener() {
 			public void buttonClick(ClickEvent event) {
 
-				validateLogin(loginForm.username.getValue(), loginForm.password.getValue());
-		}});			
+				validateEmail(loginForm.email.getValue());
+		}});
+		
+		verifyForm.loginButton.addClickListener(new Button.ClickListener() {
+			public void buttonClick(ClickEvent event) {
+				
+				validateCode(verifyForm.code.getValue());
+		}});
 	}
 	
-	public void validateLogin(String user, String pass) {
+	public void validateEmail(String email) {
 		
-		if (user.isEmpty())
+		String domain = "";
+		
+		if (email.isEmpty())
 		{
-			showError("Username field is blank.");
+			showError("Please enter you FSU CS email.");
 			return;
 		}
 		
-		if (pass.isEmpty())
-		{
-			showError("Password field is blank.");
-			return;
-		}
-			
-		try {
-			Session session = jsch.getSession(user, host, 22);
-			session.setPassword(pass);
-			
-			Properties prop = new Properties();
-			prop.put("StrictHostKeyChecking", "no");
-			session.setConfig(prop);
-	
-			session.connect();			
-			session.disconnect();
+		StringTokenizer token = new StringTokenizer(email, "@");
 	        	
-	        loginForm.close();	        
-	        checkMember(user);
-	        
-	        new CtrForum();
+		while(token.hasMoreElements())
+			domain = (String) token.nextElement();
 		
-		} catch (JSchException e) {			
-			showError("Please enter your <i>FSU CS</i> credentials");			
-			e.printStackTrace();
-			return;			
-		} 		
+		if (domain.equals("cs.fsu.edu"))
+		{
+			boolean found = checkMember(email);
+			
+			if (found == false)
+				sendEmail(email);
+			  
+			loginForm.close();	    
+
+			UI.getCurrent().addWindow(verifyForm);			
+		}
+		
+		else
+		{
+			showError("Please enter a valid FSU CS email.");
+			return;
+		}	 
+	}
+
+	public void validateCode(String code) {
+		
+		if (code.equals("GoNoles"))
+		{
+			verifyForm.close();
+			new CtrForum();
+		}
+		
+		else
+		{
+			showError("The verification code you entered is not correct.");
+			return;
+		}
 	}
 	
-	public void checkMember(String name) {
+	public boolean checkMember(String email) {
+		
+		boolean found = false;
 		
 		try {
 			dbTable = new TblMember();
-			dbTable.findMember(name);
+			found = dbTable.findMember(email);
 			
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+		
+		return found;
 	}
 	
 	public void showError(String errorString) {
 		new Notification("<center>Login Error</center>",
 			    "<br/>" + errorString,
-			    Notification.Type.WARNING_MESSAGE, true)
+			    Notification.Type.ERROR_MESSAGE, true)
 			    .show(Page.getCurrent());
+	}
+	
+	private void sendEmail(String to){	
+				
+		Properties prop = System.getProperties();
+		prop.put("mail.smtp.host", "smtp.live.com");
+		prop.put("mail.smtp.auth", true);
+		prop.put("mail.smtp.starttls.enable","true");
+		prop.put("mail.smtp.port", "587");
+		 
+		Session session = Session.getDefaultInstance(prop,
+				new javax.mail.Authenticator() {
+					protected PasswordAuthentication getPasswordAuthentication() {
+						return new PasswordAuthentication("myemail@email.com","mypassword");
+					}
+				});
+
+	    try{
+	    	Message msg = new MimeMessage(session);
+	    	
+	    	msg.setFrom(new InternetAddress("myemail@email.com"));
+	    	
+	    	msg.addRecipient(Message.RecipientType.TO,
+	    			new InternetAddress(to));
+
+	    	msg.setSubject("FSUDC Verification Code");
+
+	    	msg.setText("Welcome to FSUDC!\n\n" +
+	    	"The verification code is: GoNoles\n\n" +
+	    	"Please keep this email because you will need to enter this code each time you visit FSUDC.\n\n" +
+	    	"Have A Great Day!\n\n" +
+	    	"Students of the Web");
+
+	    	Transport.send(msg);
+	        
+	    	System.out.println("Your email has sent.");
+	    	
+	    }catch (MessagingException ex) {
+	    	ex.printStackTrace();
+	    	System.out.println("Your email did not send.");
+	    }
 	}
 }
