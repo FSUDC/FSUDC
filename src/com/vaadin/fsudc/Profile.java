67ae3c0fcce4b627dd9081294ee763f03ec86eba
ang618 <aliciag235@gmail.com>
2014-04-12 18:42:04 -0500

    Update Profile.java

--- src/com/vaadin/fsudc/Profile.java
+++ src/com/vaadin/fsudc/Profile.java
@@ -1,8 +1,54 @@
+/*
+ * File:    Profile.java
+ * Author:  Alicia Gambill
+ * Date:    April 2014
+ * Project: FSUDC
+ *  
+ * Description: The entity class that holds member profile information.
+ */
+
 package com.vaadin.fsudc;
 
 public class Profile {
 	
-	public Profile (String user) {
+	private String user;
+	private String userPic;
+	private String userBio;
+	
+	public Profile (String u) {
 		
+		setUser(u);
+		setUserPic("C:\\Users\\Alicia\\Pictures\\FSUDC\\pic1.png");
+		setUserBio("Tell us something about yourself...");
 	}
+	
+	public void setUser(String u) {
+		
+		user = u;
+	}
+	
+	public String getUser() {
+		
+		return user;
+	}	
+	
+	public void setUserPic(String pic) {
+		
+			userPic = pic;
+	}
+	
+	public String getUserPic() {
+		
+		return userPic;
+	}
+	
+	public void setUserBio(String bio) {
+		
+		userBio = bio;
+	}
+	
+	public String getUserBio() {
+		
+		return userBio;
+	}		
 }
