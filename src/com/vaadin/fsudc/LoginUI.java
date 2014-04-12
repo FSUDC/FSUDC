77b15c4fb2d06e759dfb466ebc221bba88ff8328
ang618 <aliciag235@gmail.com>
2014-04-12 18:29:55 -0500

    Update LoginUI.java

--- src/com/vaadin/fsudc/LoginUI.java
+++ src/com/vaadin/fsudc/LoginUI.java
@@ -1,3 +1,12 @@
+/*
+ * File:    LoginUI.java
+ * Author:  Alicia Gambill
+ * Date:    April 2014
+ * Project: FSUDC
+ *  
+ * Description: The user interface for login.
+ */
+
 package com.vaadin.fsudc;
 
 import com.vaadin.ui.Button;
@@ -6,26 +15,33 @@ import com.vaadin.ui.TextField;
 import com.vaadin.ui.Window;
 
 @SuppressWarnings({ "serial"})
-
 public class LoginUI extends Window {
-
-    public TextField email = new TextField();
-    public Button loginButton = new Button("Login");
-    public CustomLayout windowLayout;
+ 
+	public CustomLayout windowLayout;
+  public Button loginButton;
+  public TextField email;
 	
-    public LoginUI () {
-	
-	super("Login");    	
-    	center();
+	public LoginUI () {
+		
+		super("Login");  
+		
+		// Window position
+  	center();
     	
-    	windowLayout = new CustomLayout("loginLayout");      
-
-        windowLayout.addComponent(email, "email");
-        windowLayout.addComponent(loginButton, "login");
+  	// Initialize
+  	windowLayout = new CustomLayout("loginLayout");
+  	loginButton = new Button("Login");
+  	email = new TextField();  	
+    	      
+  	// Add components to layout
+    windowLayout.addComponent(email, "email");
+    windowLayout.addComponent(loginButton, "login");    
         
-        setContent(windowLayout);
+    // Require email and focus
+    email.setRequired(true);        
+    email.focus();
         
-        email.setRequired(true);        
-        email.focus();
-   }
+    // Set layout
+    setContent(windowLayout);
+	}
 }
