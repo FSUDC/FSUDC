ef2b80909117960538f230ab571e3156080f4ee9
ang618 <aliciag235@gmail.com>
2014-04-12 18:45:06 -0500

    Update VerifyUI.java

--- src/com/vaadin/fsudc/VerifyUI.java
+++ src/com/vaadin/fsudc/VerifyUI.java
@@ -1,3 +1,12 @@
+/*
+ * File:    VerifyUI.java
+ * Author:  Alicia Gambill
+ * Date:    April 2014
+ * Project: FSUDC
+ *  
+ * Description: The user interface that requests verification code.
+ */
+
 package com.vaadin.fsudc;
 
 import com.vaadin.ui.Button;
@@ -6,27 +15,36 @@ import com.vaadin.ui.PasswordField;
 import com.vaadin.ui.Window;
 
 @SuppressWarnings({ "serial"})
-
 public class VerifyUI extends Window {
 
-	public PasswordField code = new PasswordField();
-  public Button loginButton = new Button("Login");
-  public CustomLayout windowLayout;
-	
+	public CustomLayout windowLayout;
+	public PasswordField code;
+    public Button loginButton;
+    	
 	public VerifyUI () {
 		
 		super("Verification");    	
+		
+		// Window position
     	center();
     		
+    	// Layout
 		windowLayout = new CustomLayout("verifyLayout");      
-
+		
+		// Initialize
+		code  = new PasswordField();
+		loginButton = new Button("Login");
+		
+		// Add components
         windowLayout.addComponent(code, "code");
         windowLayout.addComponent(loginButton, "login");
         
-        setContent(windowLayout);
-        
+        // Set code require and focus
         code.setRequired(true);        
         code.focus();
+        
+        // Set layout
+        setContent(windowLayout);
 	}
 }
 	
